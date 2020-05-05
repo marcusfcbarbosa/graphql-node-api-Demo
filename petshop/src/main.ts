5cacd5f4487f51be13d350bd51a3bb4d04b79256
Your Name <you@example.com>
2020-05-05 10:15:30 -0300

    Implementando Documentacao SWagger e arquivos POSTMAN

--- petshop/src/main.ts
+++ petshop/src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
     .addTag('petshop')
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('docs', app, document);
+  //SwaggerModule.setup('docs', app, document);
 
   await app.listen(3000);
 }
